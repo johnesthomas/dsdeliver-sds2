f20855af214c38e65f2c68763a92e143ce27df24
Johnes Thomas <johnes_brasil10@hotmail.com>
2021-01-10 00:06:21 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,6 @@
-import { Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Routes';
 
@@ -6,6 +8,7 @@ function App() {
   return (
     <>
       <Routes />
+      <ToastContainer />
     </>
   );
 }
