dc0b0316e199c7e008455626adb75b04a3ba5af4
Johnes Thomas <johnes_brasil10@hotmail.com>
2021-01-10 01:14:32 -0300

    att

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { OrderLocationData, OrderPayLoad } from "./Orders/types";
+import {OrderPayLoad } from "./Orders/types";
 
 const API_URL = 'http://localhost:8080'
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
